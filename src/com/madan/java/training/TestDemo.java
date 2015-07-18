f4a4bf5e2b5e3321ace020e07156f096981c3bda
Madan Dhungana <madandhungana@gmail.com>
2015-07-18 14:46:04 +0545

    Added scanner object

--- src/com/madan/java/training/TestDemo.java
+++ src/com/madan/java/training/TestDemo.java
@@ -3,12 +3,15 @@
  */
 package com.madan.java.training;
 
+import java.util.Scanner;
+
 /**
  * @author madan_000
  *
  */
 public class TestDemo {
 public static void main(String[] args) {
+	Scanner input=new Scanner(System.in);
 	System.out.println("Demo");
 }
 }
