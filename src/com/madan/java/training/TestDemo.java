e4a394d1f7c3149d94739ec488ead745f5e81708
Madan Dhungana <madandhungana@gmail.com>
2015-07-18 14:35:57 +0545

    First Program added

---
+++ src/com/madan/java/training/TestDemo.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+package com.madan.java.training;
+
+/**
+ * @author madan_000
+ *
+ */
+public class TestDemo {
+public static void main(String[] args) {
+	System.out.println("Demo");
+}
+}
