c97e72ca4e0b5d0d4b723d65c4f6de30c4cc3a49
Madan Dhungana <madandhungana@gmail.com>
2015-07-18 14:42:40 +0545

    New java class added

---
+++ src/com/madan/java/training/HelloWorld.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package com.madan.java.training;
+
+/**
+ * @author madan_000
+ *
+ */
+public class HelloWorld {
+	
+	public static void main(String []args){
+		
+		System.out.println("Hello world");
+	}
+
+}
