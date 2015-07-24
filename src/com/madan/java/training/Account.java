1a0b26fadb66c93804b0d74ec0addc80aa88830e
Madan Dhungana <madandhungana@gmail.com>
2015-07-24 06:42:25 +0545

    account class for a user

---
+++ src/com/madan/java/training/Account.java
@@ -0,0 +1,16 @@
+package com.madan.java.training;
+
+public class Account {
+	private String name;
+	
+	//method to set name in the object
+	void setName(String name){
+		this.name=name;
+		}
+	
+	//method to get name from the object
+	String getName(){
+		return this.name;
+	}
+
+}
