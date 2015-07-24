0220d4dfb4ebff0b0f886b1801f20a5c280902a6
Madan Dhungana <madandhungana@gmail.com>
2015-07-24 14:38:21 +0545

    static Account created name parameter passed as madan age parameter set 5

---
+++ src/com/madan/java/training/TestAccount.java
@@ -0,0 +1,13 @@
+package com.madan.java.training;
+
+public class TestAccount {
+
+	static Account a=new Account();
+	public static void main(){
+		long v=5;
+		a.setName("madan");
+		a.setAge(5);
+		a.print();
+	}
+	
+}
